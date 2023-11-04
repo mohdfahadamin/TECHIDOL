8319cda5c2c5c1efb0302d570874cad9c2499cd1
Mohd Fahad Amin <mdfahadamin8@gmail.com>
2023-11-05 01:12:03 +0530

    updating the content

---
+++ admin dashboard/analytics and reports/index.js
@@ -0,0 +1,134 @@
+var ctx = document.getElementById("myChart").getContext("2d");
+var earning = document.getElementById("earning").getContext("2d");
+
+var myChart = new Chart(ctx, {
+  type: "doughnut",
+  data: {
+    labels: ["Courses", "Mananage", "paid"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [1200, 1900, 3000],
+        backgroundColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+        ],
+      },
+    ],
+  },
+  options: {
+    responsive: true,
+  },
+});
+
+var myChart = new Chart(earning, {
+  type: "bar",
+  data: {
+    labels: [
+      "january",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "Sept",
+      "Oct",
+      "Nevember",
+      "December",
+    ],
+    datasets: [
+      {
+        label: "Earning",
+        data: [
+          12000, 1000, 34000, 54000, 2000, 33000, 54000, 7000, 29000, 82000,
+          6000, 4000,
+        ],
+        backgroundColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+      },
+    ],
+  },
+  options: {
+    responsive: true,
+  },
+});
+
+const allSideMenu = document.querySelectorAll("#sidebar .side-menu.top li a");
+
+allSideMenu.forEach((item) => {
+  const li = item.parentElement;
+
+  item.addEventListener("click", function () {
+    allSideMenu.forEach((i) => {
+      i.parentElement.classList.remove("active");
+    });
+    li.classList.add("active");
+  });
+});
+
+// TOGGLE SIDEBAR
+const menuBar = document.querySelector("#content nav .bx.bx-menu");
+const sidebar = document.getElementById("sidebar");
+
+menuBar.addEventListener("click", function () {
+  sidebar.classList.toggle("hide");
+});
+
+const searchButton = document.querySelector(
+  "#content nav form .form-input button"
+);
+const searchButtonIcon = document.querySelector(
+  "#content nav form .form-input button .bx"
+);
+const searchForm = document.querySelector("#content nav form");
+
+searchButton.addEventListener("click", function (e) {
+  if (window.innerWidth < 576) {
+    e.preventDefault();
+    searchForm.classList.toggle("show");
+    if (searchForm.classList.contains("show")) {
+      searchButtonIcon.classList.replace("bx-search", "bx-x");
+    } else {
+      searchButtonIcon.classList.replace("bx-x", "bx-search");
+    }
+  }
+});
+
+if (window.innerWidth < 768) {
+  sidebar.classList.add("hide");
+} else if (window.innerWidth > 576) {
+  searchButtonIcon.classList.replace("bx-x", "bx-search");
+  searchForm.classList.remove("show");
+}
+
+window.addEventListener("resize", function () {
+  if (this.innerWidth > 576) {
+    searchButtonIcon.classList.replace("bx-x", "bx-search");
+    searchForm.classList.remove("show");
+  }
+});
+
+const switchMode = document.getElementById("switch-mode");
+
+switchMode.addEventListener("change", function () {
+  if (this.checked) {
+    document.body.classList.add("dark");
+  } else {
+    document.body.classList.remove("dark");
+  }
+});
